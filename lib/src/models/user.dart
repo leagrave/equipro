f8644ebf75d2cb199c22c23e3588140b268eeca7
leagrave <leagrave666@gmail.com>
2024-12-02 19:25:24 +0100

    Initial commit

---
+++ lib/src/models/user.dart
@@ -0,0 +1,15 @@
+
+
+class User {
+  final int id;
+  final String name;
+  final String password;
+  final String email;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.password,
+    required this.email,
+  });
+}
